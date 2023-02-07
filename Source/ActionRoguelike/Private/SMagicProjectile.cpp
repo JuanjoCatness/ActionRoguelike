1295161cc52c8bf90f9d2945de3ef1606c2e7d9a
JuanJoseGomezSimon <juanjo@catnessgames.com>
2023-02-07 11:54:53 +0100

    AI behavior tree, black board, custom tasks, EQS, PawnSensingComponent

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -17,7 +17,7 @@ ASMagicProjectile::ASMagicProjectile()
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor /*TODO poner que no sea el mismo que el instigator, dejarlo de momento para debuggear*/) {
+	if (OtherActor && OtherActor != GetInstigator()) {
 		USAttributeCompontent* AttributeComp = Cast<USAttributeCompontent>(OtherActor->GetComponentByClass(USAttributeCompontent::StaticClass()));
 		if (AttributeComp) {
 			AttributeComp->ApplyHealthChange(-20.0f);
